e53bb9337085d8dbecf27a990de414f0ab2f977f
Aditya Deshpande <aadityadeshpande@outlook.com>
2023-12-12 15:32:14 +0530

    NavBarStyles_Version1

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,35 @@
-import React from "react";
-import navbarLogo from "..//..//assets/navLogo.jpg";
-import Box from "@mui/material/Box";
-import { Typography } from "@mui/material";
+import React, { useState } from "react";
+import navbarLogo from "..//..//assets/navLogo.svg";
+import menuIcon from "..//..//assets/menu_button.svg";
+import downloadIcon from '..//..//assets/downloadIcon.svg';
+import "../Navbar/Navbar.css";
+
 const Navbar = () => {
+  const navbarLinks = ["Home", "About Me", "Projects"];
+  const [breadMenuOpen, setBreadMenuOpen] = useState(false);
+
+  const toggleBreadMenu = () => {
+    setBreadMenuOpen(!breadMenuOpen);
+  };
+
   return (
-    <Box>
-      <Typography>Hello</Typography>
-    </Box>
+    <div className={`navbar-main-container ${breadMenuOpen ? "open" : ""}`}>
+      <div className="navbar-image-container">
+        <img src={navbarLogo} className="navbar-image" alt="navbar logo" />
+      </div>
+      <div className={`navbar-links ${breadMenuOpen ? "open" : ""}`}>
+        {navbarLinks.map((links) => (
+          <a href="#">{links}</a>
+        ))}
+        <button className="navbar-btn">
+          <img className="download-img" src={downloadIcon} alt="download" />
+          <h6 className="navbar-button-text">Resume</h6>
+        </button>
+      </div>
+      <div className="navbar-breadMenu-btn" onClick={toggleBreadMenu}>
+        <img src={menuIcon} alt="Menu Icon" />
+      </div>
+    </div>
   );
 };
 
