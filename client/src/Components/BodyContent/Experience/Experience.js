6dd184ca46cf61b09783f40706ac2d592ff032f1
Aditya Deshpande <aadityadeshpande@outlook.com>
2023-12-17 23:33:19 +0530

    Experinece component in development

--- client/src/Components/BodyContent/Experience/Experience.js
+++ client/src/Components/BodyContent/Experience/Experience.js
@@ -3,8 +3,22 @@ import "../Experience/ExperienceStyles.css";
 import briefcaseIcon from "..//..//..//assets/iconBriefcase.svg";
 import educationIcon from "..//..//..//assets/iconEducation.svg";
 import rectangleIcon from "..//..//..//assets/Rectangle.svg";
-import infosysImage from '..//..//..//assets/InfosysImage.png';
+import infosysImage from "..//..//..//assets/InfosysImage.png";
+import SgbauImage from '..//..//..//assets/SGBAU.jpeg';
 const Experience = () => {
+  const skillTags = [
+    "ReactJs",
+    "React Native",
+    "JavaScript",
+    "TypeScript",
+    "HTML",
+    "CSS",
+    "UI Developement",
+    "RWD",
+    "Jira",
+    
+  ];
+
   return (
     <div className="exp-main-container">
       <h2>Experience</h2>
@@ -22,21 +36,53 @@ const Experience = () => {
         </div>
         <div className="exp-cards-container">
           <div className="exp-card">
-            <div style={{display:"flex", justifyContent:"space-between", flexDirection:"row"}}>
-              <div>
-                <p>Infosys Limited</p>
-                <p>System Engineer (ReactJs)</p>
-                <p>february 2022 - present</p>
+            <div className="exp-card-upperpart">
+              <div className="exp-upperpart-text">
+                <p className="exp-cmpname-text">Infosys Limited</p>
+                <p className="exp-position-text">System Engineer (ReactJs)</p>
+                <p className="exp-duration-text">february 2022 - present</p>
               </div>
               <div>
-                <img src={infosysImage} alt="Company Image"/>
+                <img
+                  className="ex-cmpimage"
+                  src={infosysImage}
+                  alt="Company Image"
+                />
               </div>
             </div>
             <div>
               <hr></hr>
             </div>
             <div>
-              <p>Skills Which I use Here</p>
+              <p className="exp-skills-title">Skills Which I use Here</p>
+              <div className="exp-skillchip-container">
+                {skillTags.map((tags)=>(<div className="exp-skillchip">
+                  <p className="exp-skills-text">{tags}</p>
+                </div>))}
+              </div>
+            </div>
+          </div>
+          <div className="exp-card">
+            <div className="exp-card-upperpart">
+              <div className="exp-upperpart-text">
+                <p className="exp-cmpname-text">Amravati University</p>
+                <p className="exp-position-text">Bachelor of Engineering (BE)</p>
+                <p className="exp-duration-text">2015 -2019</p>
+              </div>
+              <div>
+                <img
+                  className="ex-cmpimage"
+                  src={SgbauImage}
+                  alt="Company Image"
+                />
+              </div>
+            </div>
+            <div>
+              <hr></hr>
+            </div>
+            <div>
+              <p className="exp-edu-title">Specialization : Mechanical Engineering</p>
+              
             </div>
           </div>
         </div>
